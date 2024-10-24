2ebebedef3fb80c66c52230b54e1f314583d5219
Grid-dev01 <it@dinjit.com>
2024-10-24 18:36:14 +0530

    test

--- src/app/api/summaryapi/route.js
+++ src/app/api/summaryapi/route.js
@@ -1,17 +1,10 @@
 
-// import { MongoClient } from 'mongodb';
-// import { NextResponse } from 'next/server';
-// import { cookies } from 'next/headers';
-// const uri = 'mongodb+srv://karthikvaranasi07:l39eu8xTl4lK9stI@cluster0.vbw2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 import { MongoClient } from 'mongodb';
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
 const uri = "mongodb+srv://karthikvaranasi07:l39eu8xTl4lK9stI@cluster0.vbw2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri);
-// You can add the rest of your logic here
-
 export async function GET(req) {
     try {
         const cookieData = cookies().getAll();
