893ed761612b248a0da9a4bc7bdc85cac9e0044d
Grid-dev01 <it@dinjit.com>
2024-10-24 18:17:53 +0530

    second

--- src/app/api/summaryapi/route.js
+++ src/app/api/summaryapi/route.js
@@ -2,9 +2,7 @@
 import { MongoClient } from 'mongodb';
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
-const uri = 'mongodb+srv://karthikvaranasi07:l39eu8xTl4lK9stI@cluster0.vbw2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-mongodb+srv://karthikvaranasi07:l39eu8xTl4lK9stI@cluster0.vbw2n.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=Cluster0
-';
+const uri = 'mongodb+srv://karthikvaranasi07:l39eu8xTl4lK9stI@cluster0.vbw2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 export async function GET(req) {
     const client = new MongoClient(uri) 
     try {
